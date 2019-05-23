609d6b9b824d3f3d376bb40c313280b85c840f01
vagrant <vagrant@homestead>
2019-05-23 12:43:03 +0000

    step 1b

---
+++ database/factories/CommentFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Comment;
+use Faker\Generator as Faker;
+
+$factory->define(Comment::class, function (Faker $faker) {
+    return [
+        'message' => $faker->text,
+        'topic_id' => $faker->numberBetween(1, 5)
+    ];
+});
