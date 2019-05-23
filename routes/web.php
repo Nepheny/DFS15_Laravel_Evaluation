bc2143c32b15b84fccd4c5ee6584761f88bbc51f
vagrant <vagrant@homestead>
2019-05-23 13:26:42 +0000

    auth and step 1d

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,47 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/**
+ * Route 1
+ * Uri : /
+ * Action : appelle la méthode index du contrôleur
+ * Name : home
+ */
+
+Route::get('/', 'TopicController@index')->name('home');
+
+/**
+ * Route 2
+ * Ressource
+ * Modèle : topics
+ * Contrôleur : TopicController
+ * Sauf la route index (car elle est générée par la Route 1)
+ */
+
+Route::resource('topics', 'TopicController', ['except' => ['index']]);
+
+/**
+ * Route 3
+ * Uri : commentaire
+ * Action : appelle la méthode comment du contrôleur
+ * Name : commentaire
+ * Méthode http : post
+ */
+
+Route::post('commentaire', 'TopicController@comment')->name('commentaire');
+
+/**
+ * Route 4
+ * Uri : recherche
+ * action : appelle la méthode search du contrôleur
+ * name : recherche
+ */
+
+Route::post('recherche', 'TopicController@search')->name('recherche');
+
+/**
+ * Auth routes
+ */
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
